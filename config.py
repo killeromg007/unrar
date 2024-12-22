d664700b233e329acf73caea97f377cad45cf0ae
killeromg007 <991127smalljames@gmail.com>
2024-12-22 08:33:15 +0800

    a
    a

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///anonymous_messages.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    GOOGLE_OAUTH_CLIENT_ID = os.environ.get('GOOGLE_OAUTH_CLIENT_ID')
-    GOOGLE_OAUTH_CLIENT_SECRET = os.environ.get('GOOGLE_OAUTH_CLIENT_SECRET')
